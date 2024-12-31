503e351895c8ed2d929621196cdcfb820d10f476
Ivan Hanloth <ivan@hanloth.com>
2024-12-31 21:55:02 +0800

    简化隐藏函数

--- core/listener.py
+++ core/listener.py
@@ -10,7 +10,8 @@ class HotkeyListener():
     def __init__(self):
         try:
             ShowWindow(Config.hwnd, SW_SHOW)
-            tool.changeMute(Config.hwnd,0)
+            if Config.mute_after_hide:
+                tool.changeMute(Config.hwnd,0)
         except:
             pass
         tool.sendNotify("Boss Key正在运行！", "Boss Key正在为您服务，您可通过托盘图标看到我")
@@ -29,7 +30,6 @@ class HotkeyListener():
     def BindHotKey(self):
         self.hotkeys = {
             Config.hide_hotkey: self.onHide,
-            # Config.startup_hotkey: self.onStartup,
             Config.close_hotkey: self.Close
         }
         self.hotkeys = tool.keyConvert(self.hotkeys)
@@ -38,33 +38,30 @@ class HotkeyListener():
         self.listener.start()
 
     def onHide(self,e=""):
-        Config.hwnd_n = GetForegroundWindow()
         if Config.times == 1:
             # 隐藏窗口
+            current = GetForegroundWindow()
             if Config.send_before_hide:
                 time.sleep(0.2)
                 keyboard.Controller().tap(keyboard.KeyCode.from_vk(0xB2))
                 
-            ShowWindow(Config.hwnd_n, SW_HIDE)
+            ShowWindow(current, SW_HIDE)
             if Config.mute_after_hide:
-                tool.changeMute(Config.hwnd_n,1)
-            Config.hwnd_b=Config.hwnd_n
-            Config.hwnd=Config.hwnd_b
+                tool.changeMute(current,1)
+            Config.hwnd=current
             Config.times = 0
         else:
             # 显示窗口
-            ShowWindow(Config.hwnd_b, SW_SHOW)
+            ShowWindow(Config.hwnd, SW_SHOW)
             if Config.mute_after_hide:
-                tool.changeMute(Config.hwnd_b,0)
-            Config.hwnd_b = ""
+                tool.changeMute(Config.hwnd,0)
             Config.times = 1
         Config.save()
 
     def Close(self,e=""):
         tool.sendNotify("Boss Key已停止服务", "Boss Key已成功退出")
         if Config.times == 0:
-            ShowWindow(Config.hwnd_b, SW_SHOW)
-            Config.hwnd_b = ""
+            ShowWindow(Config.hwnd, SW_SHOW)
             Config.times = 1
             
         self.stop()
