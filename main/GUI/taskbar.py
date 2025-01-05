d48cc03c403f6db233f15c5abb5f0a42b03169c4
Ivan Hanloth <ivan@hanloth.com>
2025-01-05 15:24:16 +0800

    优化开机自启

--- main/GUI/taskbar.py
+++ main/GUI/taskbar.py
@@ -26,16 +26,16 @@ class TaskBarIcon(wx.adv.TaskBarIcon):
         self.Bind(wx.adv.EVT_TASKBAR_LEFT_DOWN, self.onLeftClick)
 
     def CreatePopupMenu(self):
-        menu = wx.Menu()
-        menu.Append(self.MENU_SETTING, '设置')
-        menu.Append(self.MENU_STARTUP, '开机自启', kind=wx.ITEM_CHECK)
-        menu.Check(self.MENU_STARTUP, tool.checkStartup("Boss Key Application",Config.file_path))
-        menu.AppendSeparator()
-        menu.Append(self.MENU_UPDATE, '检查更新')
-        menu.Append(wx.ID_ABOUT, '关于')
-        menu.AppendSeparator()
-        menu.Append(self.MENU_EXIT, '退出')
-        return menu
+        self.menu = wx.Menu()
+        self.menu.Append(self.MENU_SETTING, '设置')
+        self.menu.Append(self.MENU_STARTUP, '开机自启', kind=wx.ITEM_CHECK)
+        self.menu.Check(self.MENU_STARTUP, tool.checkStartup("Boss Key Application",Config.file_path))
+        self.menu.AppendSeparator()
+        self.menu.Append(self.MENU_UPDATE, '检查更新')
+        self.menu.Append(wx.ID_ABOUT, '关于')
+        self.menu.AppendSeparator()
+        self.menu.Append(self.MENU_EXIT, '退出')
+        return self.menu
     
     def onLeftClick(self,e=''):
         if Config.click_to_hide:
@@ -43,16 +43,20 @@ class TaskBarIcon(wx.adv.TaskBarIcon):
                 Config.HotkeyListener.onHide()
 
     def onStartup(self,e):
-        try:
-            res = tool.modifyStartup("Boss Key Application",file_path=Config.file_path)
-            if res == "Added":
-                tool.sendNotify(title="开机自启状态变化",message="Boss Key开机自启已开启")
-            elif res == "Removed":
+        if tool.checkStartup("Boss Key Application",Config.file_path):
+            if tool.removeStartup("Boss Key Application"):
                 tool.sendNotify(title="开机自启状态变化",message="Boss Key开机自启已关闭")
+                self.menu.Check(self.MENU_STARTUP,False)
             else:
-                tool.sendNotify(title="开机自启状态变化",message="Boss Key开机自启状态未知")
-        except:
-            tool.sendNotify(title="程序运行出错",message=f"Boss Key程序运行出错，请尝试重启程序")
+                tool.sendNotify(title="开机自启状态变化",message="Boss Key开机自启关闭失败")
+                self.menu.Check(self.MENU_STARTUP,True)
+        else:
+            if tool.addStartup("Boss Key Application",Config.file_path):
+                tool.sendNotify(title="开机自启状态变化",message="Boss Key开机自启已开启")
+                self.menu.Check(self.MENU_STARTUP,True)
+            else:
+                tool.sendNotify(title="开机自启状态变化",message="Boss Key开机自启开启失败")
+                self.menu.Check(self.MENU_STARTUP,False)
 
     def onSetting(self,e):
         Config.SettingWindow.RefreshLeftList()
